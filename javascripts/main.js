16dd72556c2a6c78b2f899f5a034f0c74124d3a5
Tim A. Conner <timaconner1@gmail.com>
2018-01-05 14:01:03 -0600

    Only tell those asteroids that are potentially hazaradous to fuck off.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,18 @@
 
 const model = require("./model");
 
-model.fetchSpaceObjects('2017-01-01', '2017-01-01').then((objects) => {
-    console.log(objects);
+model.fetchSpaceObjects('2017-01-01', '2017-01-02').then((data) => {
+    let objects = data.near_earth_objects;
+    let dates = Object.keys(objects);
+
+    let fuckedObjects = [];
+
+    dates.forEach(date => {
+        objects[date].forEach((spaceObject) => {
+            if(spaceObject.is_potentially_hazardous_asteroid === true){
+                fuckedObjects.push(spaceObject);
+            }
+        });
+    });
 });
 
