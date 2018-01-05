12624f6095d9747be75489454632c17de2b6a97a
Tim A. Conner <timaconner1@gmail.com>
2018-01-05 14:35:32 -0600

    Add viewFuckingPhrase.

---
+++ javascripts/view.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+const messageTemplate = require("../templates/asteroidMessage.hbs");
+const $ = require("jquery");
+
+module.exports.viewFuckingPhrases = (phrases) => {
+    $(".output").append(messageTemplate(phrases));
+};
