52cd918e7be3574e415f2336f38c99987dbb53ef
Tim A. Conner <timaconner1@gmail.com>
2018-01-05 14:51:12 -0600

    Changed class selector to id selector.

--- javascripts/view.js
+++ javascripts/view.js
@@ -5,5 +5,5 @@ const messageTemplate = require("../templates/asteroidMessage.hbs");
 const $ = require("jquery");
 
 module.exports.viewFuckingPhrases = (phrases) => {
-    $(".output").append(messageTemplate(phrases));
+    $("#output").append(messageTemplate(phrases));
 };
