d9183bff6373694397155ff773deff83018948de
Tim A. Conner <timaconner1@gmail.com>
2018-01-05 13:42:16 -0600

    Add fetchSpaceObjects.

--- javascripts/model.js
+++ javascripts/model.js
@@ -2,9 +2,15 @@
 
 const $ = require("jquery");
 
-module.exports.fetchWeatherData = (startDate, endDate) => {
-    fetchApiKey().then((data) => {
-        console.log(data);
+module.exports.fetchSpaceObjects = (startDate, endDate) => {
+    return new Promise(function(resolve) {
+        fetchApiKey().then((key) => {
+            $.ajax({
+                url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${key}`
+            }).done((objects) => {
+                resolve(objects);
+            });
+        });
     });
 };
 
