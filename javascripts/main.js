a9e427494b3cbe1692044e4c7fec3e64b8bb0eac
Tim A. Conner <timaconner1@gmail.com>
2018-01-05 14:58:55 -0600

    Pull dates from date input.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,26 +2,28 @@
 
 const model = require("./model");
 const view = require("./view");
+const $ = require("jquery");
 
-model.fetchSpaceObjects('2017-01-01', '2017-01-02').then((data) => {
-    let objects = data.near_earth_objects;
-    let dates = Object.keys(objects);
+$("#find").click(function(){
+    model.fetchSpaceObjects($("#startDate").val(), $("#startDate").val()).then((data) => {
+        let objects = data.near_earth_objects;
+        let dates = Object.keys(objects);
 
-    let objectsToFuck = [];
+        let objectsToFuck = [];
 
-    dates.forEach(date => {
-        objects[date].forEach((spaceObject) => {
-            if(spaceObject.is_potentially_hazardous_asteroid === true){
-                objectsToFuck.push(spaceObject);
-            }
+        dates.forEach(date => {
+            objects[date].forEach((spaceObject) => {
+                if(spaceObject.is_potentially_hazardous_asteroid === true){
+                    objectsToFuck.push(spaceObject);
+                }
+            });
         });
-    });
 
-
-    Promise.all([model.fetchFuckingString(objectsToFuck[0].name), model.fetchFuckingString(objectsToFuck[1].name), model.fetchFuckingString(objectsToFuck[2].name)]).then(fuckingPhrases => { 
-        // console.log(fuckingPhrases);
-        view.viewFuckingPhrases({
-            "objects": fuckingPhrases   
+        Promise.all([model.fetchFuckingString(objectsToFuck[0].name), model.fetchFuckingString(objectsToFuck[1].name), model.fetchFuckingString(objectsToFuck[2].name)]).then(fuckingPhrases => { 
+            // console.log(fuckingPhrases);
+            view.viewFuckingPhrases({
+                "objects": fuckingPhrases   
+            });
         });
     });
 });
